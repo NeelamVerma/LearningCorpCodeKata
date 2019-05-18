a84b6db12048fafe4725d1610c067cd37a99eb25
NeelamVerma <neelzverma@gmail.com>
2019-05-17 23:28:39 -0400

    NV - Table view modifications

---
+++ LearningCorpCode/LearningCorpCode/NYCSchoolConstants.swift
@@ -0,0 +1,16 @@
+//
+//  NYCSchoolConstants.swift
+//  LearningCorpCode
+//
+//  Created by Neelam Verma on 17/05/19.
+//  Copyright © 2019 Neelam Verma. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    static let NYC_SCHOOL_URL = "https://data.cityofnewyork.us/resource/s3k6-pzi2.json"
+    static let NYC_SCHOOL_SAT_DETAIL_URL = "https://data.cityofnewyork.us/resource/f9bf-2cp4.json"
+    static let NYC_SCHOOL_CELL_IDENTIFIER = "nycSchoolCell"
+}
+
---
+++ LearningCorpCode/LearningCorpCode/SchoolService.swift
@@ -0,0 +1,75 @@
+//
+//  SchoolService.swift
+//  LearningCorpCode
+//
+//  Created by Neelam Verma on 17/05/19.
+//  Copyright © 2019 Neelam Verma. All rights reserved.
+//
+
+import UIKit
+
+class SchoolService: NSObject {
+    
+    //MARK:- Pagination Vars
+    var pageOffset = 0
+    var limit = 10
+    
+    //MARK:- Network Vars
+    var session = URLSession.shared
+    var nycSchoolURL = Constants.NYC_SCHOOL_URL
+    var nycSchoolSatDetailURL = Constants.NYC_SCHOOL_SAT_DETAIL_URL
+    
+    //MARK:- Network APIS
+    /// To fetch NYC high Schools page by page , per page limit is set to 10
+    /// https://data.cityofnewyork.us/resource/s3k6-pzi2.json?$limit=10&$offset=0
+    /// - Parameters:
+    ///   - pageNumber: page offset
+    ///   - completion: is all data gets fetched
+    ///   - failure: failure
+    func fetchListOfNYCSchools(pageNumber: Int = 0, completion: @escaping (Bool) -> (), failure: @escaping (String) -> ()) {
+        guard var urlComponents = URLComponents(string: nycSchoolURL) else {
+            self.resetPageOffsetToPrevious(failure: failure, error: "Unexpected error")
+            return
+        }
+        urlComponents.query = "$limit=\(limit)&$offset=\(pageOffset)"
+        guard let url = urlComponents.url else {
+            self.resetPageOffsetToPrevious(failure: failure, error: "Unexpected error")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let dataTask = session.dataTask(with: request) { (schoolListData, response, error) in
+            
+            guard let data = schoolListData else {
+                self.resetPageOffsetToPrevious(failure: failure, error: error?.localizedDescription)
+                return
+            }
+            do {
+                let listOfSchools =  try JSONDecoder().decode([NYCSchool].self, from: data)
+                if listOfSchools.count < self.limit {
+                    completion(false)
+                } else {
+                    completion(true)
+                }
+            } catch {
+                self.resetPageOffsetToPrevious(failure: failure, error: error.localizedDescription)
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+    //MARK:- Helper Methods
+    /// Reset The page index to the previous if the current page fails to fetch next NYC Schools
+    /// - Parameters:
+    ///   - failure: failure
+    ///   - error: error messsgae
+    fileprivate func resetPageOffsetToPrevious(failure: (String) -> (), error: String?){
+        if self.pageOffset > 0 {
+            self.pageOffset = self.pageOffset - self.limit
+        }
+        failure("\(error ?? "Unexpected error:").")
+    }
+
+}
