92cbed36c8374efd29c51e7ffef41d49a8a288e8
NeelamVerma <neelzverma@gmail.com>
2019-05-17 23:06:59 -0400

    NV - Intial code for models

---
+++ LearningCorpCode/LearningCorpCode/Model/NYCSchool.swift
@@ -0,0 +1,23 @@
+//
+//  NYCSchool.swift
+//  LearningCorpCode
+//
+//  Created by Neelam Verma on 17/05/19.
+//  Copyright © 2019 Neelam Verma. All rights reserved.
+//
+
+import UIKit
+
+struct NYCSchool: Decodable {
+    var dbn: String?
+    var school_name: String?
+    var school_email: String?
+    var phone_number: String?
+    var total_students: String?
+    var website: String?
+    var primary_address_line_1: String?
+    var state_code: String?
+    var city: String?
+    var zip: String?
+}
+
---
+++ LearningCorpCode/LearningCorpCode/ViewController/Cell/NYCSchoolCell.swift
@@ -0,0 +1,18 @@
+//
+//  NYCSchoolCell.swift
+//  LearningCorpCode
+//
+//  Created by Neelam Verma on 17/05/19.
+//  Copyright © 2019 Neelam Verma. All rights reserved.
+//
+
+import UIKit
+
+class NYCSchoolCell: UITableViewCell {
+    
+    @IBOutlet weak var schoolNameLabel: UILabel!
+    @IBOutlet weak var schoolEmailLabel: UILabel!
+    @IBOutlet weak var schoolPhoneNumber: UILabel!
+    @IBOutlet weak var totalStudentsLabel: UILabel!
+    
+}
