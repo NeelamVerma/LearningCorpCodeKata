c21dbc5f6ace0a89e492ac7597ac209b1e8ff5e2
NeelamVerma <neelzverma@gmail.com>
2019-05-18 23:32:59 -0400

    NV - Tests for service class

--- LearningCorpCode/LearningCorpCodeTests/SchoolServiceTests.swift
+++ LearningCorpCode/LearningCorpCodeTests/SchoolServiceTests.swift
@@ -25,6 +25,118 @@ class SchoolServiceTests: XCTestCase {
         mockSession = nil
         super.tearDown()
     }
+    
+    //MARK:- session task tests
+    func test_get_request_with_URL() {
+        guard var urlComponents = URLComponents(string: Constants.NYC_SCHOOL_URL) else {
+            return
+        }
+        urlComponents.query = "$limit=\(subject.limit)&$offset=\(subject.pageOffset)"
+        let url: URL? = urlComponents.url
+        subject.fetchListOfNYCSchools(completion: { ( list , isMore) in }) { error in }
+        XCTAssert(mockSession.lastURL == url)
+    }
+    
+    func test_URL_shouldhave_limit_and_offset_query_withdefault() {
+        subject.fetchListOfNYCSchools(completion: { ( list , isMore) in }) { error in }
+        XCTAssert(mockSession.lastURL?.query == "$limit=10&$offset=0")
+    }
+    
+    func test_URL_shouldhave_limit_and_offset_query_withSPecific_page() {
+        
+        subject.limit = 50
+        subject.pageOffset = 15
+        subject.fetchListOfNYCSchools(completion: { ( list , isMore) in }) { error in }
+        XCTAssert(mockSession.lastURL?.query == "$limit=50&$offset=15")
+    }
+    
+    func test_get_resume_called() {
+        let dataTask = MockURLSessionDataTask()
+        mockSession.nextDataTask = dataTask
+        subject.fetchListOfNYCSchools(completion: { ( list , isMore) in }) { error in }
+        XCTAssert(dataTask.resumeWasCalled)
+    }
+    
+    //MARK:- func fetchSATScoresOfNYCSchool(:) tests
+    func test_should_return_success() {
+        let expectedData = "[{\"dbn\":\"01M292\",\"num_of_sat_test_takers\":\"29\",\"sat_critical_reading_avg_score\":\"355\",\"sat_math_avg_score\":\"404\",\"sat_writing_avg_score\":\"363\",\"school_name\":\"HENRY STREET SCHOOL FOR INTERNATIONAL STUDIES\"}\n,{\"dbn\":\"01M448\",\"num_of_sat_test_takers\":\"91\",\"sat_critical_reading_avg_score\":\"383\",\"sat_math_avg_score\":\"423\",\"sat_writing_avg_score\":\"366\",\"school_name\":\"UNIVERSITY NEIGHBORHOOD HIGH SCHOOL\"}\n]".data(using: .utf8)
+        mockSession.nextData = expectedData
+        var error: String?
+        subject.fetchListOfNYCSchools(completion: { ( list , isMore) in }) { errorMessage in
+            error = errorMessage
+        }
+        XCTAssertNil(error)
+    }
+    
+    func test_should_return_array_of_school() {
+        let expectedData = "[{\"dbn\":\"01M292\",\"num_of_sat_test_takers\":\"29\",\"sat_critical_reading_avg_score\":\"355\",\"sat_math_avg_score\":\"404\",\"sat_writing_avg_score\":\"363\",\"school_name\":\"HENRY STREET SCHOOL FOR INTERNATIONAL STUDIES\"}\n,{\"dbn\":\"01M448\",\"num_of_sat_test_takers\":\"91\",\"sat_critical_reading_avg_score\":\"383\",\"sat_math_avg_score\":\"423\",\"sat_writing_avg_score\":\"366\",\"school_name\":\"UNIVERSITY NEIGHBORHOOD HIGH SCHOOL\"}\n]".data(using: .utf8)
+        mockSession.nextData = expectedData
+        
+        subject.fetchListOfNYCSchools(completion: { ( list , isMore) in
+            XCTAssertTrue(list.count > 0)
+            XCTAssertTrue(list.count == 2)
+        }) { error in }
+        
+    }
+    
+    
+    func test_should_show_fail_if_invalid_url() {
+        
+        subject.nycSchoolURL = "&%$$$"
+        var error: String?
+        subject.fetchListOfNYCSchools(completion: { ( _ , _) in}) { errorMessage in
+            error = errorMessage
+        }
+        XCTAssertNotNil(error)
+        XCTAssertEqual(error, "Unexpected error.")
+    }
+    
+    func test_should_show_fail_if_Data_is_nil() {
+        mockSession.nextData = nil
+        var error: String?
+        subject.fetchListOfNYCSchools(completion: { ( _ , _) in
+        }) { errorMessage in
+            error = errorMessage
+        }
+        XCTAssertNotNil(error)
+    }
+    
+    func test_should_show_fail_if_Data_is_NOT_nil_NOT_able_to_DECODE() {
+        mockSession.nextData = "[{\"dbn123\":\"01M292\",\"num_of_sat_test_takers1\":\"29\",\"sat_critical_reading_avg_score\":\"355\",\"sat_math_avg_score\":\"4}]".data(using: .utf8)
+        var error: String?
+        subject.fetchListOfNYCSchools(completion: { ( _ , _) in
+        }) { errorMessage in
+            error = errorMessage
+        }
+        XCTAssertNotNil(error)
+    }
+    
+    //MARK:- Pagination Tests
+    
+    func test_should_disbale_isMore_when_all_data_get_fetched() {
+        let expectedData = "[{\"dbn\":\"01M292\",\"num_of_sat_test_takers\":\"29\",\"sat_critical_reading_avg_score\":\"355\",\"sat_math_avg_score\":\"404\",\"sat_writing_avg_score\":\"363\",\"school_name\":\"HENRY STREET SCHOOL FOR INTERNATIONAL STUDIES\"}\n,{\"dbn\":\"01M448\",\"num_of_sat_test_takers\":\"91\",\"sat_critical_reading_avg_score\":\"383\",\"sat_math_avg_score\":\"423\",\"sat_writing_avg_score\":\"366\",\"school_name\":\"UNIVERSITY NEIGHBORHOOD HIGH SCHOOL\"}\n]".data(using: .utf8)
+        mockSession.nextData = expectedData
+        var isMoreData: Bool?
+        subject.fetchListOfNYCSchools(completion: { ( _ , isMore) in
+            isMoreData = isMore
+        }) { errorMessage in }
+        
+        XCTAssertTrue(isMoreData == false)
+    }
+    
+    func test_should_enable_isMore_when_all_data_get_fetched() {
+        let expectedData = "[{\"dbn\":\"01M292\",\"num_of_sat_test_takers\":\"29\",\"sat_critical_reading_avg_score\":\"355\",\"sat_math_avg_score\":\"404\",\"sat_writing_avg_score\":\"363\",\"school_name\":\"HENRY STREET SCHOOL FOR INTERNATIONAL STUDIES\"}\n,{\"dbn\":\"01M448\",\"num_of_sat_test_takers\":\"91\",\"sat_critical_reading_avg_score\":\"383\",\"sat_math_avg_score\":\"423\",\"sat_writing_avg_score\":\"366\",\"school_name\":\"UNIVERSITY NEIGHBORHOOD HIGH SCHOOL\"},{\"dbn\":\"01M449\",\"num_of_sat_test_takers\":\"91\",\"sat_critical_reading_avg_score\":\"388\",\"sat_math_avg_score\":\"423\",\"sat_writing_avg_score\":\"366\",\"school_name\":\"UNIVERSITY NEIGHBORHOOD HIGH SCHOOL\"}\n]".data(using: .utf8)
+        mockSession.nextData = expectedData
+        mockSession.nextError = nil
+        
+        subject.limit = 2
+        var isMoreData: Bool?
+        subject.fetchListOfNYCSchools(completion: { ( _ , isMore) in
+            isMoreData = isMore
+        }) { errorMessage in }
+        
+        XCTAssertTrue(isMoreData == true)
+    }
 }
 
 //MARK:- Mock Session
@@ -35,8 +147,8 @@ class MockURLSession: URLSession {
     var nextDataTask = MockURLSessionDataTask()
     var nextData: Data?
     var nextError: Error?
-    
     private (set) var lastURL: URL?
+    
     override func dataTask(with request: URLRequest, completionHandler: @escaping completionHandler) -> URLSessionDataTask {
         lastURL = request.url
         
